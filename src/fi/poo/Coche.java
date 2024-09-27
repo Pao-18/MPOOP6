9f186017eeb283c99017e7741c435b4f2549161a
Pao-18 <143154582+Pao-18@users.noreply.github.com>
2024-09-26 19:59:41 -0600

    Creación de práctica

---
+++ src/fi/poo/Coche.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.poo;
+
+/**
+ *
+ * @author poo03alu09
+ */
+public class Coche {
+   private String color;
+   private String marca;
+   private String placa;
+   
+    public Coche(){
+    }
+
+    public Coche(String color, String marca, String placa) {
+        this.color = color;
+        this.marca = marca;
+        this.placa = placa;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void acelerar(){
+        System.out.println("acelerar");
+    }
+    
+    public void frenar(){
+        System.out.println("frenar");
+    }
+    /**
+     * metodo que carga cosas
+     * @param cosa cantidad de carga
+     * @return cosa que cargo el coche
+     */
+    public String carga(String cosa){
+        System.out.println("cargando "+cosa);
+       return cosa;
+    }
+    
+    @Override
+    public String toString() {
+        return "Coche{" + "color=" + color + ", marca=" + marca + ", placa=" + placa + '}';
+    }
+   
+}
+
---
+++ src/fi/poo/POOP6.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.poo;
+
+/**
+ *
+ * @author poo03alu09
+ */
+public class POOP6 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        
+        Coche coche = new Coche();
+        Coche coche2 = new Coche("rojo", "ford", "MXN-435");
+        System.out.println(coche);
+        System.out.println(coche2);
+        
+    
+    
+    
+    }
+}
+
